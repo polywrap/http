49f974fae2b96d5cec4b0245b8b82de5169f3ec7
Cesar <cesarbrazon10@gmail.com>
2023-03-29 20:14:49 +0200

    chore: import add headers of polywrap client

--- implementations/swift/Package.swift
+++ implementations/swift/Package.swift
@@ -15,17 +15,25 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "PolywrapClient", path: "../../../../polywrap/swift-client"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HttpPlugin",
-            dependencies: []
+            dependencies: ["PolywrapClient"],
+            cSettings: [
+                .headerSearchPath("../../include")
+            ]
         ),
         .testTarget(
             name: "HttpPluginTests",
-            dependencies: ["HttpPlugin"]),
+            dependencies: ["HttpPlugin"],
+            cSettings: [
+                .headerSearchPath("../../include")
+            ]
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
