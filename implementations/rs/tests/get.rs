147b91cce75d8d9a784eb24b925506ba1174dbf5
Cesar <cesarbrazon10@gmail.com>
2023-04-27 19:05:32 +0200

    chore(implementation/rust): add test of get with url params

--- implementations/rs/tests/get.rs
+++ implementations/rs/tests/get.rs
@@ -10,11 +10,14 @@ use polywrap_client::{
         },
         uri::Uri,
     },
-    msgpack::msgpack,
-    plugin::{package::PluginPackage, JSON::from_str},
+    msgpack::{msgpack, serialize},
+    plugin::{package::PluginPackage, Map, JSON},
 };
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::BTreeMap,
+    sync::{Arc, Mutex},
+};
 
 fn get_client() -> PolywrapClient {
     let http_plugin = HttpPlugin {};
@@ -53,29 +56,51 @@ fn simple_get() {
         .unwrap();
     assert_eq!(response.status, 200);
     assert_ne!(response.body, None);
-    let body: ExpectedResponse = from_str(&response.body.unwrap()).unwrap();
+    let body: ExpectedResponse = JSON::from_str(&response.body.unwrap()).unwrap();
     assert_eq!(body.id, 1);
 }
 
-// #[test]
-// fn params_get() {
-//     let response = get_client()
-//         .invoke::<HttpResponse>(
-//             &Uri::try_from("plugin/http").unwrap(),
-//             "get",
-//             Some(&msgpack!({
-//                 "url": "https://jsonplaceholder.typicode.com/todos",
-//                 "request": {
-//                     "urlParams": {
-//                         "id": 1
-//                     }
-//                 }
-//             })),
-//             None,
-//             None,
-//         )
-//         .unwrap();
-//     assert_eq!(response.status, 200);
-//     assert_ne!(response.body, None);
-//     dbg!(&response.body);
-// }
+#[derive(Debug, Serialize, Deserialize)]
+struct GetArgs {
+    url: String,
+    request: Request,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Request {
+    #[serde(rename = "urlParams")]
+    url_params: Map<String, String>,
+    #[serde(rename = "responseType")]
+    response_type: String,
+}
+
+#[test]
+fn params_get() {
+    let mut params = Map(BTreeMap::new());
+    params.0.insert("id".to_string(), "1".to_string());
+
+    let args = GetArgs {
+        url: "https://jsonplaceholder.typicode.com/todos".to_string(),
+        request: Request {
+            url_params: params,
+            response_type: "TEXT".to_string(),
+        },
+    };
+    let response = get_client()
+        .invoke::<HttpResponse>(
+            &Uri::try_from("plugin/http").unwrap(),
+            "get",
+            Some(&serialize(args).unwrap()),
+            None,
+            None,
+        )
+        .unwrap();
+    assert_eq!(response.status, 200);
+    assert_ne!(response.body, None);
+    let response: JSON::Value = JSON::from_str(response.body.unwrap().as_str()).unwrap();
+    if let JSON::Value::Array(r) = response {
+        assert_eq!(r.len(), 1)
+    } else {
+        panic!("Error in params_get test")
+    }
+}
