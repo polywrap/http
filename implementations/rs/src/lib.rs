73e65c05f19c1b75fc945ca57e8d178f7b3b8f8d
Cesar <cesarbrazon10@gmail.com>
2023-06-17 01:06:00 +0200

    chore: move methods from mapping to its own file & update client version

--- implementations/rs/src/lib.rs
+++ implementations/rs/src/lib.rs
@@ -1,18 +1,24 @@
-use crate::wrap::wrap_info::get_manifest;
-use mapping::{parse_request, parse_response};
+use crate::{wrap::wrap_info::get_manifest, parse_request::parse_request, parse_response::parse_response};
 use multipart::client::lazy::Multipart;
 use polywrap_core::invoker::Invoker;
 use polywrap_plugin::{error::PluginError, implementor::plugin_impl, JSON};
+use ureq::{Request as UreqRequest, Response as UreqResponse};
 use std::{io::Cursor, sync::Arc};
 use wrap::{
     module::{ArgsGet, ArgsPost, Module},
-    types::{Response, ResponseType},
+    types::{Response, ResponseType, FormDataEntry},
 };
-pub mod mapping;
 pub mod wrap;
+pub mod parse_response;
+pub mod parse_request;
+
+pub enum RequestMethod {
+    GET,
+    POST,
+}
 
 #[derive(Debug)]
-pub struct HttpPlugin {}
+pub struct HttpPlugin;
 
 #[plugin_impl]
 impl Module for HttpPlugin {
@@ -21,7 +27,7 @@ impl Module for HttpPlugin {
         args: &ArgsGet,
         _: Arc<dyn Invoker>,
     ) -> Result<Option<Response>, PluginError> {
-        let response = parse_request(&args.url, args.request.clone(), mapping::RequestMethod::GET)
+        let response = parse_request(&args.url, args.request.clone(), RequestMethod::GET)
             .unwrap()
             .call()
             .map_err(|e| PluginError::InvocationError {
@@ -47,7 +53,7 @@ impl Module for HttpPlugin {
         let request = parse_request(
             &args.url,
             args.request.clone(),
-            mapping::RequestMethod::POST,
+            RequestMethod::POST,
         )
         .unwrap();
 
@@ -59,45 +65,9 @@ impl Module for HttpPlugin {
 
         let response = if let Some(r) = &args.request {
             if let Some(body) = &r.body {
-                let value = JSON::from_str::<JSON::Value>(body.as_str());
-                if let Ok(json) = value {
-                    request
-                        .send_json(json)
-                        .map_err(|e| PluginError::InvocationError {
-                            exception: e.to_string(),
-                        })?
-                } else {
-                    return Err(PluginError::JSONError(value.unwrap_err()));
-                }
+                handle_json(request, body)?
             } else if let Some(form_data) = &r.form_data {
-                let mut multipart = Multipart::new();
-                for entry in form_data.iter() {
-                    if entry._type.is_some() {
-                        if let Some(v) = &entry.value {
-                            let buf = base64::decode(v).unwrap();
-                            let cursor = Cursor::new(buf);
-                            let file_name = if let Some(f) = &entry.file_name {
-                                Some(f.as_str())
-                            } else {
-                                None
-                            };
-                            multipart.add_stream(entry.name.as_str(), cursor, file_name, None);
-                        };
-                    } else {
-                        if let Some(v) = &entry.value {
-                            multipart.add_text(entry.name.as_str(), v);
-                        };
-                    }
-                }
-                // Send the request with the multipart/form-data
-                let mdata = multipart.prepare().unwrap();
-                request
-                    .set(
-                        "Content-Type",
-                        &format!("multipart/form-data; boundary={}", mdata.boundary()),
-                    )
-                    .send(mdata)
-                    .unwrap()
+                handle_form_data(request, form_data)?
             } else {
                 request.call().map_err(|e| PluginError::InvocationError {
                     exception: e.to_string(),
@@ -111,5 +81,51 @@ impl Module for HttpPlugin {
         let parsed_response = parse_response(response, response_type)?;
 
         Ok(Some(parsed_response))
+    }
+}
+
+fn handle_form_data(request: UreqRequest, form_data: &Vec<FormDataEntry>) -> Result<UreqResponse, PluginError> {
+    let mut multipart = Multipart::new();
+    for entry in form_data.iter() {
+        if entry._type.is_some() {
+            if let Some(v) = &entry.value {
+                let buf = base64::decode(v).unwrap();
+                let cursor = Cursor::new(buf);
+                let file_name = if let Some(f) = &entry.file_name {
+                    Some(f.as_str())
+                } else {
+                    None
+                };
+                multipart.add_stream(entry.name.as_str(), cursor, file_name, None);
+            };
+        } else {
+            if let Some(v) = &entry.value {
+                multipart.add_text(entry.name.as_str(), v);
+            };
+        }
+    }
+    // Send the request with the multipart/form-data
+    let mdata = multipart.prepare().unwrap();
+    request
+        .set(
+            "Content-Type",
+            &format!("multipart/form-data; boundary={}", mdata.boundary()),
+        )
+        .send(mdata)
+        .map_err(|e| PluginError::InvocationError {
+            exception: e.to_string(),
+        })
+}
+
+fn handle_json(request: UreqRequest, body: &String) -> Result<UreqResponse, PluginError> {
+    let value = JSON::from_str::<JSON::Value>(body.as_str());
+    if let Ok(json) = value {
+        request
+            .send_json(json)
+            .map_err(|e| PluginError::InvocationError {
+                exception: e.to_string(),
+            })
+    } else {
+        return Err(PluginError::JSONError(value.unwrap_err()));
     }
 }
---
+++ implementations/rs/src/parse_response.rs
@@ -0,0 +1,40 @@
+use std::collections::BTreeMap;
+
+use polywrap_plugin::{Map, error::PluginError};
+
+use crate::wrap::types::{ResponseType, Response};
+
+pub fn parse_response(
+    response: ureq::Response,
+    encoding: ResponseType,
+) -> Result<Response, PluginError> {
+    let headers = response
+        .headers_names()
+        .iter()
+        .map(|header_name| {
+            (
+                header_name.to_string(),
+                response.header(header_name).unwrap().to_string(),
+            )
+        })
+        .collect::<BTreeMap<String, String>>();
+    let headers = Map(headers);
+    let status = response.status();
+    let status_text = response.status_text().to_string();
+
+    let mut reader = response.into_reader();
+    let mut data = vec![];
+    reader.read_to_end(&mut data).unwrap();
+
+    let data = match encoding {
+        ResponseType::BINARY => base64::encode(data),
+        _ => String::from_utf8_lossy(&data).to_string(),
+    };
+
+    Ok(Response {
+        status: status.into(),
+        status_text,
+        headers: Some(headers),
+        body: Some(data),
+    })
+}
