52c6d57369e5616c997e4cecb881302c97bd4275
Cesar <cesarbrazon10@gmail.com>
2023-07-26 19:01:05 +0200

    chore(swift): add podspec & ci

---
+++ HttpPlugin.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+    s.name             = 'HttpPlugin'
+    s.version          = '0.0.1'
+    s.summary          = 'Http plugin'
+    s.homepage         = 'https://github.com/polywrap/http'
+    s.license          = 'MIT'
+    s.author           = { 'Cesar' => 'cesar@polywrap.io' }
+  
+    s.source_files = 'implementations/swift/Source/**/*.swift'
+    s.swift_version  = "5.0"
+    s.ios.deployment_target = '13.0'
+    s.osx.deployment_target = '10.15'
+    s.source = { :git => "https://github.com/polywrap/http.git", :branch => 'feat/swift-implementation' }
+    s.static_framework = true
+    s.dependency 'PolywrapClient', '~> 0.0.4'
+  end
---
+++ implementations/swift/Source/wrap/Types.swift
@@ -0,0 +1,63 @@
+public struct ArgsGet: Codable {
+    var url: String
+    var request: Request?
+
+    public init(url: String, request: Request? = nil) {
+        self.url = url
+        self.request = request
+    }
+}
+
+public struct ArgsPost {
+    var url: String
+    var request: Request?
+
+    public init(url: String, request: Request? = nil) {
+        self.url = url
+        self.request = request
+    }
+}
+
+public struct Response: Codable {
+    var status: Int
+    var statusText: String
+    var headers: [String: String]?
+    var body: String?
+}
+
+public struct Request: Codable {
+    var headers: [String: String]?
+    var urlParams: [String: String]?
+    var responseType: ResponseType
+    var body: String?
+    var formData: [FormDataEntry]?
+    var timeout: UInt32?
+
+    public init(
+        headers: [String: String]? = nil,
+        urlParams: [String: String]? = nil,
+        responseType: ResponseType,
+        body: String? = nil,
+        formData: [FormDataEntry]? = nil,
+        timeout: UInt32? = nil
+    ) {
+        self.headers = headers
+        self.urlParams = urlParams
+        self.responseType = responseType
+        self.body = body
+        self.formData = formData
+        self.timeout = timeout
+    }
+}
+
+public enum ResponseType: Codable {
+    case TEXT
+    case BINARY
+}
+
+public struct FormDataEntry: Codable {
+    var name: String
+    var value: String?
+    var fileName: String?
+    var type: String?
+}
