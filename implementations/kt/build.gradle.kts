2a3ca68433b4b7c46220d268db0f9d57646d491b
krisbitney <kris@dorg.tech>
2023-08-14 16:04:11 -0500

    fixed kotlin plugin publishing and dependencies

--- implementations/kt/build.gradle.kts
+++ implementations/kt/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    id("com.android.library") version "8.2"
+    kotlin("multiplatform") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
     id("org.jetbrains.dokka") version "1.8.20"
@@ -10,29 +11,53 @@ group = "io.polywrap"
 version = "0.10.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
-    maven {
-        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    }
-}
-
-dependencies {
-    implementation("io.polywrap:polywrap-client:0.10.0-SNAPSHOT")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("io.ktor:ktor-client-core:2.3.2")
-    implementation("io.ktor:ktor-client-android:2.3.2")
-    testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    implementation("io.ktor:ktor-client-mock:2.3.2")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvm {
+        jvmToolchain(17)
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    androidTarget {
+        publishLibraryVariants("release")
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("io.polywrap:polywrap-client:0.10.0-SNAPSHOT")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("io.ktor:ktor-client-core:2.3.2")
+                implementation("io.ktor:ktor-client-android:2.3.2")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+                implementation("io.ktor:ktor-client-mock:2.3.2")
+            }
+        }
+    }
+}
+
+android {
+    namespace = "io.polywrap.httpPlugin"
+    compileSdk = 32
+    defaultConfig.minSdk = 24
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    testOptions {
+        unitTests.all {
+            it.enabled = false
+        }
+    }
 }
 
 // javadoc generation for Maven repository publication
